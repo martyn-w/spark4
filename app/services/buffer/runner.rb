eb19687049794a56a7dafdd1104c04c8047f18f6
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-10-31 20:44:25 +0000

    refactor reader to merge in related relationship data

--- app/services/buffer/runner.rb
+++ app/services/buffer/runner.rb
@@ -9,41 +9,37 @@ module Buffer
       Settings.buffers.each do |buffer_setting|
         next if buffer_setting.enabled == false
 
-        data = Nokogiri::XML('<data/>')
-
         reader = Reader.new(buffer_setting.source)
-        reader.read do |items|
-          # <api:object category="user"...
-          case buffer_setting.mode
-          when 'write_items'
+
+        case buffer_setting.mode
+        when 'write_items'
+          data = Nokogiri::XML('<data/>')
+          reader.read do |items|
             data.root << items
-          when 'buffer_related_items'
+          end
+          write_items(data, buffer_setting.filename)
+
+        when 'buffer_related_items'
+          reader.read do |items|
             items.each do |object_item|
-              related_data = Nokogiri::XML('<data/>')
-              related_item_url = object_item.at_xpath(buffer_setting.related_item.api.endpoint.xsl).value
-              filename = object_item.xpath(buffer_setting.related_item.filename.xsl)
-              item_reader = Reader.new(buffer_setting.related_item)
-              item_reader.read(related_item_url) do |related_items|
-                # <api:relationship id="246137" type-id="8" type="publication-user-authorship"
-                related_data.root << related_items
+              buffer_setting.related_item.tap do |related_settings|
+                data = Nokogiri::XML::Document.new
+                data.add_child(object_item)
+
+                related_item_url = data.root.at_xpath(related_settings.api.endpoint.xsl).value
+                filename = data.root.xpath(related_settings.filename.xsl)
+                item_reader = Reader.new(related_settings)
+                item_for = data.root.at_xpath(related_settings.data_for)
+
+                item_reader.read(related_item_url) do |related_items|
+                  item_for << related_items
+                end
+
+                write_items(data, filename)
               end
-              write_items(related_data, filename)
             end
           end
         end
-
-        if buffer_setting.mode == 'write_items'
-          write_items(data, buffer_setting.filename)
-        end
-
-        # byebug
-
-        # case buffer_setting.mode
-        # when 'write_items'
-        #   write_items(reader.items, buffer_setting.filename)
-        # when 'buffer_related_items'
-        #   puts 'need to do something here for buffer_related_items'
-        # end
       end
     end
 
